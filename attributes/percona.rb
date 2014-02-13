b3813388e8d24274b6db9f5d460339d2743d8d68
Sean OMeara <someara@opscode.com>
2014-02-13 18:52:25 -0500

    Updating test harness. Adding Amazon Linux support

--- attributes/percona.rb
+++ attributes/percona.rb
@@ -1,5 +1,10 @@
 default['yum']['percona']['description'] = 'Percona MySQL and tools repository'
-default['yum']['percona']['baseurl'] = "http://repo.percona.com/centos/#{node['platform_version'].to_i}/os/$basearch/"
+case node['platform']
+when 'amazon'
+  default['yum']['percona']['baseurl'] = 'http://repo.percona.com/centos/6/os/$basearch/'
+else
+  default['yum']['percona']['baseurl'] = "http://repo.percona.com/centos/#{node['platform_version'].to_i}/os/$basearch/"
+end
 default['yum']['percona']['gpgkey'] = 'http://www.percona.com/downloads/RPM-GPG-KEY-percona'
 default['yum']['percona']['gpgcheck'] = true
 default['yum']['percona']['enabled'] = true
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,16 +1,11 @@
 require 'spec_helper'
 
 describe 'yum-percona::default' do
-  context 'yum-perconas::default uses default attributes' do
-    let(:chef_run) { ChefSpec::Runner.new(:step_into => ['yum_repository']).converge(described_recipe) }
+  context 'yum-percona::default uses default attributes' do
+    let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
     it 'creates yum_repository[percona]' do
       expect(chef_run).to create_yum_repository('percona')
     end
-
-    it 'steps into yum_repository and creates template[/etc/yum.repos.d/percona.repo]' do
-      expect(chef_run).to render_file('/etc/yum.repos.d/percona.repo')
-    end
-
   end
 end
