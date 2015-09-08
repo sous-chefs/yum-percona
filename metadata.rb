673c584da5df2ac34c4746833a08479103ea52b1
Tim Smith <tsmith84@gmail.com>
2015-09-07 22:05:41 -0700

    Add OS support and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,10 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.1'
 
 depends 'yum', '~> 3.0'
+
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/yum-percona' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-percona/issues' if respond_to?(:issues_url)
