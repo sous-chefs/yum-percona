957eab7d0234a6844b9f482e56c353d0617c5d8f
Tim Smith <tsmith84@gmail.com>
2015-09-07 22:14:31 -0700

    Rubocop fixes

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'yum-percona'
-maintainer       'Chef Software, Inc.'
+name 'yum-percona'
+maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
-description      'Installs/Configures yum-percona'
+license 'Apache 2.0'
+description 'Installs/Configures yum-percona'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version '0.2.1'
 
 depends 'yum', '~> 3.0'
 
