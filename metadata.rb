feacf34a1105b6b342db0903dec027ef85c1d171
Tim Smith <tsmith84@gmail.com>
2015-09-21 15:17:32 -0700

    Require yum 3.2 and improve description

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,11 @@ name 'yum-percona'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures yum-percona'
+description 'Installs and configures the Percona Yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.1'
 
-depends 'yum', '~> 3.0'
+depends 'yum', '~> 3.2'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
   supports os
