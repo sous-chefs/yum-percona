598b1e1e91037a562ec6ef19718929a732361aad
Tim Smith <tsmith@chef.io>
2016-10-03 23:16:24 -0700

    Replace yum dependency with compat_resource

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs and configures the Percona Yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
-depends 'yum', '>= 3.2', '< 5.0'
+depends 'compat_resource', '>= 12.14.1'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
   supports os
@@ -15,4 +15,4 @@ end
 source_url 'https://github.com/chef-cookbooks/yum-percona'
 issues_url 'https://github.com/chef-cookbooks/yum-percona/issues'
 
-chef_version '>= 12'
+chef_version '>= 12.1'
