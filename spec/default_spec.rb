4cfbc02dc879a20a3e6010e24bbf49c86ec9f514
Tim Smith <tsmith@chef.io>
2017-06-14 15:48:56 -0700

    Avoid deprecation warnings in chefspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'yum-percona::default' do
   context 'yum-percona::default uses default attributes' do
-    let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
+    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
     it 'creates yum_repository[percona]' do
       expect(chef_run).to create_yum_repository('percona')
