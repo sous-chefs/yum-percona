7ee7f063790349c40583c70c0f09fcc0e4410f2c
Tim Smith <tsmith@chef.io>
2018-02-16 13:29:03 -0800

    Require Chef 12.14+ and remove the compat_resource dep
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,10 @@ description 'Installs and configures the Percona Yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.1'
 
-depends 'compat_resource', '>= 12.16.3'
-
 %w(amazon centos fedora oracle redhat scientific).each do |os|
   supports os
 end
 
 source_url 'https://github.com/chef-cookbooks/yum-percona'
 issues_url 'https://github.com/chef-cookbooks/yum-percona/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.14' if respond_to?(:chef_version)
