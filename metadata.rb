f9904c86fbb2a34294d44e73e5790074b9785b8f
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:10:27 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures the Percona Yum repository'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.0'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
@@ -12,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/yum-percona'
 issues_url 'https://github.com/chef-cookbooks/yum-percona/issues'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 12.14'
