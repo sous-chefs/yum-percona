d6e5842fee9f9c26b96ce818a97581710b495d5c
Ryan <kennedy@osuosl.org>
2021-03-10 09:12:41 -0800

    Remove centos 8 and fedora due to dnf

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,3 +1,10 @@
-describe command('yum -y install percona-release') do
-  its('exit_status') { should eq 0 }
+describe yum.repo('percona') do
+  it { should exist }
+  it { should be_enabled }
+  its('baseurl') { should cmp "http://repo.percona.com/centos/#{os.name == 'amazon' ? '7' : os.release.to_i}/os/x86_64/" }
+end
+
+describe ini("/etc/yum.repos.d/percona.repo") do
+  its("percona.gpgcheck") { should cmp 1 }
+  its("percona.gpgkey") { should cmp 'http://www.percona.com/downloads/RPM-GPG-KEY-percona' }
 end
